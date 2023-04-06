7f842443b07a83ab3f07629da802cb731d5d121b
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-06 16:17:23 -0400

    user changes

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,4 +8,18 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :course,
+        primary_key: :id, 
+        foreign_key: :instructor_id, 
+        class: :Course
+    
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id, 
+        class: :Enrollment
+
+    has_many :enrolled_courses,
+            through: :enrollments
+            source: :course
+
 end
